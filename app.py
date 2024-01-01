53a538ab1b07b854277f6f6793a5517241abe9d9
kazuki <kaz.tokyo.tech20@gmail.com>
2024-01-01 14:17:06 +0900

    feat: swallow training loss

--- app.py
+++ app.py
@@ -4,29 +4,42 @@ from scipy.interpolate import interp1d
 import numpy as np
 
 # CSVファイルを読み込む
-data = pd.read_csv('swallow-13b-llm-jp.csv')
+file_paths: dict[str, str] = {
+    'Swallow 7B': 'swallow-7b.csv',
+    'Swallow 13B': 'swallow-13b.csv',
+    'Swallow 70B': 'swallow-70b.csv'
+}
 
-# x軸とy軸のデータを処理する
-data['x'] = data['iteration'] / 250
-data['y'] = data['training-loss']  # yのデータはそのまま使用
+dataframes = {}
 
-# 補間関数を作成
-f = interp1d(data['x'], data['y'], kind='cubic')
+# Read each CSV file and convert iterations to tokens
+for model, file_path in file_paths.items():
+    df = pd.read_csv(file_path)
+    df['tokens'] = df['iteration'] * 4096 * 1024  # Convert iterations to tokens
+    dataframes[model] = df
 
-# 新しいx軸のポイントを作成
-new_x = np.linspace(data['x'].min(), data['x'].max(), num=1000, endpoint=True)
-
-# プロットの作成
+# Create the plot
 plt.figure(figsize=(10, 6))
-plt.plot(new_x, f(new_x), color='#3599db', linestyle='-', linewidth=2)  # 青色の線でプロット
 
-# 軸の範囲の設定
-plt.xlim(0, 25000 / 250 + 2)
-plt.ylim(1.3, 2.5)
+# Plot the training loss for each model
+for model, df in dataframes.items():
+    plt.plot(df['tokens'], df['training-loss'], label=model)
 
-# 軸のラベルを設定
-plt.xlabel('Billon of tokens', fontsize=15)
-plt.ylabel('Training loss', fontsize=15)
+# Set x-axis ticks to be at every 20 billion tokens
+token_ticks = np.arange(0, max(df['tokens'].max() for df in dataframes.values()), 20e9)  # 20e9 tokens = 20B tokens
+plt.xticks(token_ticks, [f"{int(tick/1e9)}B" for tick in token_ticks], fontsize=20)
 
-# グラフの表示
+# Adding labels, title, and grid
+plt.xlabel('Billions of tokens', fontsize=22)  # X軸のラベルの文字サイズを設定
+plt.ylabel('Training loss', fontsize=22)
+# plt.title('Training loss for Swallow models over tokens')
+plt.legend()
+plt.grid(True)
+plt.ylim(1.2, 3)
+plt.xlim(0, 110e9)
+plt.legend(fontsize=20)
+plt.yticks(fontsize=20)
+
+# Show the plot
+plt.tight_layout()  # Adjust layout
 plt.show()
