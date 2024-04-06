ef0023c62c5868fe367e092d410cb6758b56a20b
kazuki <kaz.tokyo.tech20@gmail.com>
2024-04-06 20:06:32 +0900

    feat: tflops

--- flops/llama-2-34b.sh
+++ flops/llama-2-34b.sh
@@ -2,11 +2,11 @@
 
 python flops/llama-2-architecutre.py \
   --hidden-size 8192 \
-  --num-layers 80 \
+  --num-layers 48 \
   --vocab-size 43176 \
   --batch-size 1024 \
   --seq-len 4096 \
-  --intermediate-size 28672 \
+  --intermediate-size 22016 \
   --num-query-groups 8 \
   --iteration 1 \
   --num-attention-heads 64
