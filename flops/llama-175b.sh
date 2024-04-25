72c1a66acc8213f5a383a98b3723cd623f28f698
kazuki <kaz.tokyo.tech20@gmail.com>
2024-04-25 13:16:31 +0900

    feat: geniac model

--- flops/llama-175b.sh
+++ flops/llama-175b.sh
@@ -4,9 +4,9 @@ python flops/llama-2-architecutre.py \
   --hidden-size 12288 \
   --num-layers 96 \
   --vocab-size 100000 \
-  --batch-size 1536 \
+  --batch-size 1728 \
   --seq-len 4096 \
-  --intermediate-size 33792 \
+  --intermediate-size 32768 \
   --num-query-groups 16 \
   --iteration 1 \
   --num-attention-heads 96
