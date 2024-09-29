7199350c110afd12fc1a4384356e5686339e21dc
kazuki <kaz.tokyo.tech20@gmail.com>
2024-09-30 01:19:48 +0900

    feat: colm24 poster

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,5 @@
-import japanize_matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib import markers
-from matplotlib.pylab import f
 
 # データの準備
 data = {
@@ -18,19 +15,30 @@ df["Billions of tokens"] = df["iteration"] * 4096 * 1024 / 1e9
 
 # 表の作成
 plt.figure(figsize=(10, 6))
-plt.plot(df["Billions of tokens"], df["Swallow-7B"], label="Swallow-7b", marker="o")
-plt.plot(df["Billions of tokens"], df["Swallow-13B"], label="Swallow-13b", marker="o")
-plt.plot(df["Billions of tokens"], df["Swallow-70B"], label="Swallow-70b", marker="o")
+plt.plot(df["Billions of tokens"], df["Swallow-7B"], label="Swallow-7b", marker="o", linewidth=3)
+plt.plot(df["Billions of tokens"], df["Swallow-13B"], label="Swallow-13b", marker="o", linewidth=3)
+plt.plot(df["Billions of tokens"], df["Swallow-70B"], label="Swallow-70b", marker="o", linewidth=3)
 
 # グラフのタイトルと軸ラベルの設定
-plt.xlabel("Billions of tokens", fontsize=26, fontweight=900)
-plt.ylabel("Japanese Score (mean)", fontsize=26, fontweight=900)
+plt.xlabel("Billions of tokens", fontsize=30)
+plt.ylabel("Japanese Score (mean)", fontsize=30)
 plt.grid(True)
-plt.ylim(0.30, 0.57)
+plt.ylim(0.25, 0.60)
 plt.xlim(-1.5, 110)
-plt.legend(fontsize=20)
-plt.yticks(fontsize=20, fontweight=900)
-plt.xticks(fontsize=20, fontweight=900)
+plt.yticks(fontsize=25)
+plt.xticks(fontsize=25)
+
+# 凡例の位置を右下に設定
+plt.legend(fontsize=24, loc='lower right', bbox_to_anchor=(1, -0.03))
+
+# 0B tokensと100B tokensのスコアを表示
+plt.text(2, df["Swallow-7B"].iloc[0] + 0.02, f'{df["Swallow-7B"].iloc[0]:.4f}', fontsize=15, va='bottom', ha='left')
+plt.text(2, df["Swallow-13B"].iloc[0] + 0.02, f'{df["Swallow-13B"].iloc[0]:.4f}', fontsize=15, va='bottom', ha='left')
+plt.text(2, df["Swallow-70B"].iloc[0] + 0.02, f'{df["Swallow-70B"].iloc[0]:.4f}', fontsize=15, va='bottom', ha='left')
+
+plt.text(103, df["Swallow-7B"].iloc[-1], f'{df["Swallow-7B"].iloc[-1]:.4f}', fontsize=15, va='bottom', ha='right')
+plt.text(103, df["Swallow-13B"].iloc[-1], f'{df["Swallow-13B"].iloc[-1]:.4f}', fontsize=15, va='bottom', ha='right')
+plt.text(103, df["Swallow-70B"].iloc[-1], f'{df["Swallow-70B"].iloc[-1]:.4f}', fontsize=15, va='bottom', ha='right')
 
 plt.tight_layout()
 plt.show()
